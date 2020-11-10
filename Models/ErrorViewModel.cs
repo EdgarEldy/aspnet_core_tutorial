b85303a19c1ebca5ecc856380c38d1c32805c42b
Edgar Eldy <ed.eldy21@live.com>
2020-11-10 21:18:35 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_core_tutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
