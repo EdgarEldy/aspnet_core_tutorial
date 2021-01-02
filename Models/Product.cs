b7d191068fe6cdd00e7a4061d97a6a658bf2fd3d
EdgarEldy <ed.eldy21@live.com>
2021-01-03 00:17:09 +0200

    Update Product Model : Add columns

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,13 @@ namespace aspnet_core_tutorial.Models
 {
     public class Product
     {
+        [Key]
+        public int Id { get; set; }
+        public Category Category { get; set; }
+        [Column(TypeName ="varchar(255)")]
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public float UnitPrice { get; set; }
     }
 }
