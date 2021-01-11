82eed2b6d0f709586eef558e4361c7d59576375a
Edgar Eldy <ed.eldy21@Live.com>
2021-01-11 23:47:44 +0200

    Add Order Model

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet_core_tutorial.Models
+{
+    public class Order
+    {
+    }
+}
