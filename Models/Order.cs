eb1d0f71a1852c8ed55182540ef580e5ad94bb3c
Edgar Eldy <ed.eldy21@Live.com>
2021-01-11 23:59:13 +0200

    Update Order Model : Add columns

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace aspnet_core_tutorial.Models
 {
     public class Order
     {
+        [Key]
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+        public float total { get; set; }
+
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
     }
 }
