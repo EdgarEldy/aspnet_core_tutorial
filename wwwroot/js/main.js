9a5c251356a7586af8a0699cf25622bc37ea76b8
Edgar Eldy <ed.eldy21@live.com>
2022-08-13 13:01:41 +0200

    Get products by category id

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,1 +1,14 @@
 ﻿// Handles ajax requests
+// Get products by category id
+$(function () {
+    $('#CategoryId').on('change', function () {
+        var id = $(this).val();
+        //alert(id);
+        $.get('/Orders/GetProducts', {id: id}, function (data) {
+            $('#ProductId').empty();
+            $.each(data, function (index, row) {
+                $('#ProductId').append("<option value='" + row.Id + "'>" + row.ProductName + "</option>")
+            });
+        });
+    });
+});
