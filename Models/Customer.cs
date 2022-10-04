0dab50d13f9502a29dfe62e541321bfdc0b1a7ef
EdgarEldy <ed.eldy21@live.com>
2022-10-04 16:19:14 +0200

    Add Customer properties

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,4 +1,7 @@
-﻿namespace aspnet_core_tutorial.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet_core_tutorial.Models
 {
     public class Customer
     {
@@ -7,5 +10,23 @@
         {
             
         }
+        
+        // Customer properties goes here..
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Tel { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+        
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
