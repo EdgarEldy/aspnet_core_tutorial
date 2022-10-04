c8942f3bf5bfeebc0463e01fc22f50285a7cbc87
EdgarEldy <ed.eldy21@live.com>
2022-10-04 16:19:27 +0200

    Add Order properties

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,4 +1,7 @@
-﻿namespace aspnet_core_tutorial.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace aspnet_core_tutorial.Models
 {
     public class Order
     {
@@ -7,5 +10,21 @@
         {
             
         }
+        
+        // Order properties goes here
+        [Key]
+        public int Id { get; set; }
+
+        public int? CustomerId { get; set; }
+
+        public int? ProductId { get; set; }
+
+        public double Qty { get; set; }
+
+        public double Total { get; set; }
+        
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
