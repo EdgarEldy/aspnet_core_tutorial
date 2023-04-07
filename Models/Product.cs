66febe4282ae1555b27d665e5f778d0a0edd55b9
Edgar Eldy <ed.eldy21@live.com>
2023-04-07 09:22:13 +0200

    Add Product model

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace aspnet_core_tutorial.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        // Constructor
+        public Product()
+        {
+        }
+    }
+}
