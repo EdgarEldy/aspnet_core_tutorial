168894c82a7ee35c5e61ada9088f537e2c694d30
Edgar Eldy <ed.eldy21@live.com>
2023-04-09 22:11:31 +0200

    Add CategorySeeder

---
+++ Seeders/CategorySeeder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using aspnet_core_tutorial.Data;
+using aspnet_core_tutorial.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aspnet_core_tutorial.Seeders
+{
+    public static class CategorySeeder
+    {
+        public static void Seed(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            try
+            {
+                context.Database.EnsureCreated();
+
+                var categories = context.Categories.FirstOrDefault();
+                if (categories == null)
+                {
+                    context.Categories.AddRange(
+                                        new Category
+                                        {
+                                            CategoryName = "Lemonades"
+                                        });
+
+                    context.SaveChanges();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
