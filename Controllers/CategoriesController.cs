ac119cc4951af9ba2c16dbea956761f0415a9ecd
Edgar Eldy <ed.eldy21@live.com>
2023-04-12 15:16:17 +0200

    Add CategoriesController and get categories list

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using aspnet_core_tutorial.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_core_tutorial.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // Initialize database context using dependency injection
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return View(categories);
+        }
+    }
+}
