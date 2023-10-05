3ba9b1ce78c3d3666a1c45ddfb0b7df97c793df2
EdgarEldy <ed.eldy21@live.com>
2023-10-05 11:38:35 +0200

    Change to SqliteConnection default connection string

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("SqliteConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
