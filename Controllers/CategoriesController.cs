e0ee9cada6b449f2920f8d5a7be20cfaeb535911
EdgarEldy <ed.eldy21@live.com>
2023-10-06 11:49:58 +0200

    Get the category to remove

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -102,5 +102,23 @@ namespace aspnet_core_tutorial.Controllers
             }
             return View(category);
         }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
     }
 }
