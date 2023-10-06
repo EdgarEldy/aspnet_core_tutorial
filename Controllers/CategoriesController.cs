0928493b746bb26b8c37d37432254546549bed16
EdgarEldy <ed.eldy21@live.com>
2023-10-06 11:03:11 +0200

    GET: Categories/Edit/id

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -22,7 +22,7 @@ namespace aspnet_core_tutorial.Controllers
             var categories = await _context.Categories.ToListAsync();
             return View(categories);
         }
-        
+
         // GET: Categories/Create
         public IActionResult Create()
         {
@@ -39,6 +39,23 @@ namespace aspnet_core_tutorial.Controllers
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
             }
 
             return View(category);
