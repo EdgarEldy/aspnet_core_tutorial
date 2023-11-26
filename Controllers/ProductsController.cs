425c4b05e5020933a8c4c1a97a6d575a31252f1a
EdgarEldy <ed.eldy21@live.com>
2023-11-26 20:06:37 +0200

    Initialize database context

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using aspnet_core_tutorial.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace aspnet_core_tutorial.Controllers
 {
     public class ProductsController : Controller
     {
+        // Initialize database context
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET
         public IActionResult Index()
         {
