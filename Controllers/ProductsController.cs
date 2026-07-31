88905b70cde3d97aca777e5779e5f4119c9acf97
EdgarEldy <ed.eldy21@live.com>
2026-07-31 20:42:23 +0400

    feat(auth): require Admin role to manage Products, keep listing public
    Same policy as Categories: Index stays [AllowAnonymous], writes
    require the Admin role.

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,12 +3,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using aspnet_core_tutorial.Data;
 using aspnet_core_tutorial.Models;
+using aspnet_core_tutorial.Seeders;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace aspnet_core_tutorial.Controllers
 {
+    [Authorize(Roles = RoleSeeder.Admin)]
     public class ProductsController : Controller
     {
         private const int PageSize = 10;
@@ -22,6 +25,7 @@ namespace aspnet_core_tutorial.Controllers
         }
 
         // GET: Products
+        [AllowAnonymous]
         public async Task<IActionResult> Index(string? searchString, int pageNumber = 1)
         {
             // Get products with categories
